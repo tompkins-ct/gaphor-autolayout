b252f900abba2a017bf205e35a44530b4c3ba729
Casey Tompkins <tompkins@kairospower.com>
2025-02-24 14:26:54 -0800

    Updated npm packaging (try to remove error issue)

--- gaphor_autolayout/elkrunner.js
+++ gaphor_autolayout/elkrunner.js
@@ -1,14 +1,27 @@
-const process = require('node:process');
+// const process = require('node:process');
 
 const ELK = require('elkjs')
 const elk = new ELK()
 
-const args = process.argv.slice(2);
-let json = JSON.parse(args[0])
-
-function process_layout(json) {
-  console.log(JSON.stringify(json));
+function elk_gen() {
+  return new ELK()
 }
 
-elk.layout(json).then(process_layout).catch(console.log);
+// const args = process.argv.slice(2);
+// let json = JSON.parse(args[0])
+//
+// function process_layout(json) {
+//   console.log(JSON.stringify(json));
+// }
+//
+// elk.layout(json).then(process_layout).catch(console.log);
 
+function  layout_json(json) {
+  const result = elk.layout(json)
+  return result.toString()
+}
+
+module.exports = {
+  elk_gen,
+  layout_json
+}
