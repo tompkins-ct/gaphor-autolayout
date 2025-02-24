88b791642ec6474d15c8fbc86008a79dcc49f2e9
Casey Tompkins <tompkins@kairospower.com>
2025-02-24 15:11:38 -0800

    update readme and return js script

--- gaphor_autolayout/elkrunner.js
+++ gaphor_autolayout/elkrunner.js
@@ -7,21 +7,21 @@ function elk_gen() {
   return new ELK()
 }
 
-// const args = process.argv.slice(2);
-// let json = JSON.parse(args[0])
-//
-// function process_layout(json) {
-//   console.log(JSON.stringify(json));
+const args = process.argv.slice(2);
+let json = JSON.parse(args[0])
+
+function process_layout(json) {
+  console.log(JSON.stringify(json));
+}
+
+elk.layout(json).then(process_layout).catch(console.log);
+
+// function  layout_json(json) {
+//   const result = elk.layout(json)
+//   return result.toString()
 // }
 //
-// elk.layout(json).then(process_layout).catch(console.log);
-
-function  layout_json(json) {
-  const result = elk.layout(json)
-  return result.toString()
-}
-
-module.exports = {
-  elk_gen,
-  layout_json
-}
+// module.exports = {
+//   elk_gen,
+//   layout_json
+// }
